c7166b86dd97f40c1ea945c4b6cdee956428708c
daksh1821 <e22cseu1238@bennett.edu.in>
2025-09-30 22:11:58 +0530

    first api build

--- server.ts
+++ server.ts
@@ -1,1 +1,9 @@
-console.log("api is working");
+import app from "./src/app.ts";
+
+const startserver = ()=>{
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, ()=>{
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+startserver();
